253c4de71bfbf63eddfe8482c9b1107790cb6633
Reuben Ninan <intelphysic@gmail.com>
2021-11-17 21:30:53 -0500

    created an extraction script, and an extracted raw.ta file for include dependency extraction

---
+++ extraction.py
@@ -0,0 +1,44 @@
+from os import listdir
+from os.path import isfile, join, isdir
+
+cFiles = []
+cLinks = {}
+
+def getAllFilesRecursive(root):
+    files = [join(root,f) for f in listdir(root) if isfile(join(root,f))] 
+    dirs = [ d for d in listdir(root) if isdir(join(root,d))]
+    for d in dirs:
+        files_in_d = getAllFilesRecursive(join(root,d))
+        if files_in_d:
+            for f in files_in_d:
+                # if file has a .c or .h extension we should add it
+                file_ext = f[-2:]
+                files.append(join(root,f))
+                if (file_ext == '.c' or file_ext == '.h'):
+                   cFiles.append(f)
+                   print('$INSTANCE',f[1:],'cFile')
+    return files
+
+def getIncludeDependencies(source_files):
+    for source_file in source_files:
+        try:
+            temp_file = open(source_file, 'r')
+            try:
+                for line in temp_file:
+                    if "#include" in line:
+                        parts = line.split(" ")
+                        dependency = parts[1].strip().split('\t')[0]
+                        cLinks[source_file] = dependency[1:-1]
+                        print('cLinks',source_file[1:], dependency[1:-1])
+            except UnicodeDecodeError:
+                pass
+        except FileNotFoundError:
+            pass
+
+
+getAllFilesRecursive('./postgresql-13.4')
+
+getIncludeDependencies(cFiles)
+
+# print(cLinks)
+
